85a05f19fa1c0815ac7a1b12cefdf0ea2ba12aff
unknown <Katherine2bk@gmail.com>
2021-04-29 18:22:26 -0700

    created a start script for node in package.json, set up home route in server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 const express = require("express")
-
+//cors, helmet, morgan
 const server = express()
+server.use(express.json())
 
-// DO YOUR MAGIC
+//router, middleware
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the API')
+})
 
 module.exports = server
